b01a4d93361dd8b7f30b282eca1135d5d1fb99cc
Bastiaan <bastiaangrisel@gmail.com>
2011-11-22 21:16:42 +0100

    Autocomplete functie met nummer en artiesten

---
+++ site/SpotifyAutoComplete.php
@@ -0,0 +1,38 @@
+<?php
+// error_reporting(0);
+require_once("libraries/metatune/config.php");
+
+$res = "noquery";
+
+if(isset($_GET["query"])){
+	$res = array();
+	$query = $_GET["query"];
+	$spotify = MetaTune::getInstance();
+	
+	try {
+	    $tracks = $spotify->searchTrack($query);
+	    
+  	    // Check for hits. MetaTune#searchTrack returns empty array in case of no result. 
+  	    if (count($tracks) < 1){
+  			$res = array();
+  	    } else {
+  			$res['query'] = $query;
+ 		
+  			foreach($tracks as $track){
+  				if(is_array($track->getArtist())){ //If multiple artists
+  					foreach($track->getArtist() as $artist){
+  						if(strpos(strtolower($artist), strtolower($query)))
+  							$res['suggestions'][] = $track->getTitle() . " | " . $artist;
+  					}
+  				}else{
+  					$res['suggestions'][] = $track->getTitle() . " | " . $track->getArtist();
+  				}	
+  				$res['data'][] = $track->getURL();
+  			}
+  	    }
+	} catch (MetaTuneException $ex) {
+		$res = "error";
+	}
+	echo json_encode($res);
+}
+
--- site/script.js
+++ site/script.js
@@ -1,13 +1,26 @@
 $(function(){
-	
+	var defaulttext = "Type an artist or track";
 	$("#nojs").hide();
 	$("#content").show();
-	$("a.play").hide();
+	// 	$("a.play").hide();
+	// 	
+	// 	$("li").hover(function(){
+	// 		$(".play", this).show();
+	// 	},function(){
+	// 		$(".play", this).hide();
+	// 	});
 	
-	$("li").hover(function(){
-		$(".play", this).show();
-	},function(){
-		$(".play", this).hide();
+	$("#tracksearch").attr("value", defaulttext);
+	$("#tracksearch").css("color","gray").css("font-style","italic");
+	$("#tracksearch").click(function(){ 
+		$(this).css("color","black").css("font-style","normal"); 
+		if($(this).attr("value") == defaulttext) $(this).attr("value", "");
 	});
 	
+	var options = { 
+		serviceUrl:'SpotifyAutoComplete.php', 
+		maxHeight: 360,
+		width: 300
+		};
+	var a = $('#tracksearch').autocomplete(options);
 });
