4691c847243ca0a1834c2490ec0ae2f1284d8252
Herman Banken <hermanbanken@gmail.com>
2011-11-25 15:24:28 +0100

    Added EchoNest API submodule

---
+++ site/moods.js
@@ -0,0 +1,8 @@
+$(function(){
+	var moods = ["happy", "angry","sad", "relaxing", "excited"];
+	
+	function moodbox(name, i){
+		var b = $("<div class='mood mood-"+name+" mood-"+i+"'></div>");
+		
+	}
+});
