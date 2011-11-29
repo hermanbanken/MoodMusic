ca9ae30e9f0a6febb5f1313a903ac92e828a814c
Bastiaan <bastiaangrisel@gmail.com>
2011-11-29 15:25:36 +0100

    Site update

---
+++ site/moods.js
@@ -0,0 +1,18 @@
+$(function(){
+$('#globe').tagcanvas({textColour : '#000000'});
+	if(!$('#globe').tagcanvas({
+		textColour : '#ffffff',
+		textHeight : 35,
+		textFont : "Lucida Sans Unicode, sans-serif",
+		maxSpeed : 0.1,
+		outlineColour : "#ffffff",
+		outlineThickness : 2,
+		frontSelect : true,
+		reverse : true,
+		shadowBlur : 1,
+		shadowOffset : [1,1]
+	})) {
+		// TagCanvas failed to load
+		$('#myCanvasContainer').hide();
+	}
+});
